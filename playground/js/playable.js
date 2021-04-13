7f7b6c99219f32f1912dd1c24e472334d1c79c66
mbett <mbett@cs.cmu.edu>
2021-04-13 13:07:20 -0400

    Update playable.js

--- playground/js/playable.js
+++ playground/js/playable.js
@@ -6,20 +6,16 @@ var reset = document.getElementById('reset');
 var jsCode = textareaJS.value;
 
 function fillCode() {
-       console.log('okay1');
     editable.textContent = textareaJS.value;
     section.innerHTML = ' ';
-    console.log('okay2');
+
     try {
         
       eval(editable.textContent);
-           console.log('okay3');
     } catch(e) {
-           console.log('e====' + e);
       let para = document.createElement('p');
       para.textContent = e;
       section.appendChild(para);
-           console.log('okay5');
         
     }
 }
