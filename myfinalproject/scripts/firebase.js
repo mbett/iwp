d33116c67a8ed976261ddebeed08b406c0ed0b9a
mbett <mbett@cs.cmu.edu>
2021-04-20 14:08:25 -0400

    Update firebase.js

--- myfinalproject/scripts/firebase.js
+++ myfinalproject/scripts/firebase.js
@@ -10,10 +10,13 @@ function initializeFirebase() {
     appId: "1:1096908921815:web:06b956148d358dbd36c992",
     measurementId: "G-ZFPVE55QKH"
   };
-  // Initialize Firebase
+  // Initialize Firebase uses the configuration values above
   firebase.initializeApp(firebaseConfig);
+  
+  // Allow analytics to run
   firebase.analytics();
 }
+
 
 function writeStudentAnswers(name, email, answers) {
   firebase.database().ref('users/' + name).set({
@@ -24,17 +27,16 @@ function writeStudentAnswers(name, email, answers) {
     firebase.database().ref('deck/flashy').set(myFlashcards);
 }
 
+// Make the initial connection to Firebase using our key
 initializeFirebase();
 // writeStudentAnswers("michael","foo@bar.com", ["b","c","e","f"]);
 
+// Set the base directory of the data
 let fc = firebase.database().ref('deck/myfinalproject');
-console.log('Output: ' + fc.toString() );
-console.log('JSON: ' + fc.toJSON() );
 
-let qd;
+// Retrieve and print out the JSON string to the console
 fc.on("value", function(retrieve) {
     let queryData = retrieve.val();
     console.log('Real JSON:' + JSON.stringify(queryData) );
     qd = JSON.stringify(queryData);
 });
-console.log('Real Real JSON:' + qd );
