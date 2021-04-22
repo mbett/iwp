355ce893b0713c4b3ac1203e454630f184c06fe8
mbett <mbett@cs.cmu.edu>
2021-04-22 16:21:56 -0400

    Update loadpage.js

--- myfinalproject/scripts/loadpage.js
+++ myfinalproject/scripts/loadpage.js
@@ -127,7 +127,7 @@ function showQuestion(number, question) {
        // print the distractors
        const distractor = question.distractor;
        for (i=0; i < distractor.length; ++i) {
-           html +='<input type="radio" name="' + i + '" onchange=checkAnswer("' + number + '", "' + distractor[i] + '") value="' + distractor[i] + '">' + distractor[i] + ' <br/>';
+           html +='<input type="radio" name="' + i + '" onchange=checkAnswer(' + number + ', '\' + distractor[i] + '\') value='\' + distractor[i] + \'>' + distractor[i] + ' <br/>';
        }
        html +='<input type="radio" name="' + distractor.length + '" onchange=checkAnswer("' + number + '", "' + question.answer + '") value="' + question.answer + '">' + question.answer + ' <br/>';
 
